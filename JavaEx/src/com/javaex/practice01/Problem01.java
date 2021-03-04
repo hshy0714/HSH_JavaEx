cad4e4bb3dc734a176b9c3d41a86ce35361baa52
황순형 <hshy0714@naver.com>
2021-03-04 23:34:47 +0900

    Practice01-1

--- JavaEx/src/com/javaex/practice01/Problem01.java
+++ JavaEx/src/com/javaex/practice01/Problem01.java
@@ -3,17 +3,12 @@ package com.javaex.practice01;
 public class Problem01 {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		//	풀이 코드 작성
-
-=======
 		// 1 ~ 100 수 중에서 5의 배수, 7의 배수만 출력
 		for (int num = 1; num <= 100; num++) {
 			if (num % 5 == 0 && num % 7 == 0) {
 				System.out.println(num);
 			}
 		}
->>>>>>> branch 'master' of https://github.com/hshy0714/HSH_JavaEx.git
 	}
 
 }
