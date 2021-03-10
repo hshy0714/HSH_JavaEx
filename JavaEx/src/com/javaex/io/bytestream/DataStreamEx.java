39bc89bcf372943ed3c7af0b8a412141182e8e89
황순형 <hshy0714@naver.com>
2021-03-10 17:09:33 +0900

    Byte 기반 스트림

---
+++ JavaEx/src/com/javaex/io/bytestream/DataStreamEx.java
@@ -0,0 +1,13 @@
+package com.javaex.io.bytestream;
+
+public class DataStreamEx {
+	//	바이트 기반 스트림은 기본 데이터 타입을 저장할 수 없다
+	//	DateInputStream
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
