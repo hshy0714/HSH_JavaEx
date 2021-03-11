496855869fe70f516edb77b82ebe069bc7df8f2f
A <A@DESKTOP-RV1FBSB>
2021-03-12 00:41:00 +0900

    미니 프로젝트

---
+++ AddressBook/src/com/java/phone/PhoneBook.java
@@ -0,0 +1,29 @@
+package com.java.phone;
+
+import java.util.Scanner;
+
+public class PhoneBook {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		boolean run = true;
+				
+		while(run) {
+			System.out.println("******************************");
+			System.out.println("*       전화번호 관리 프로그램          *");
+			System.out.println("******************************");
+			System.out.println(" 1.리스트 2.등록 3.삭제 4.검색 5.종료  ");
+			
+			System.out.println(">메뉴 번호");
+
+			swtich(menu) {
+				case 1:	//	리스트
+					System.out.println();
+				
+			}
+		}
+
+	}
+
+}
