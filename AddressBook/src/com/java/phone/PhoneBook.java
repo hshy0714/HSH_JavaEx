e32b7253bdbca255537b348d18d332a2b9a0c01f
A <A@DESKTOP-RV1FBSB>
2021-03-16 01:06:21 +0900

    Delete - 배열로 진행되는 로직으로 변경

--- AddressBook/src/com/java/phone/PhoneBook.java
+++ AddressBook/src/com/java/phone/PhoneBook.java
@@ -1,29 +1,40 @@
 package com.java.phone;
 
-import java.util.Scanner;
-
 public class PhoneBook {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		boolean run = true;
-				
-		while(run) {
-			System.out.println("******************************");
-			System.out.println("*       전화번호 관리 프로그램          *");
-			System.out.println("******************************");
-			System.out.println(" 1.리스트 2.등록 3.삭제 4.검색 5.종료  ");
-			
-			System.out.println(">메뉴 번호");
-
-			swtich(menu) {
-				case 1:	//	리스트
-					System.out.println();
-				
-			}
-		}
-
+	private String name;
+	private String hp;
+	private String tel;
+	
+	public PhoneBook(String name, String hp, String tel) {
+		super();
+		this.name = name;
+		this.hp = hp;
+		this.tel = tel;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getHp() {
+		return hp;
+	}
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
 	}
 
+	@Override
+	public String toString() {
+		return name+","+hp+","+tel;
+	}
 }
+
