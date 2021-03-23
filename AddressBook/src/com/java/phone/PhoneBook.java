17a3234a4fac046aa26c42b6b9ebb9260b093d1a
황순형 <hshy0714@naver.com>
2021-03-23 09:31:35 +0900

    Repository Sync

---
+++ AddressBook/src/com/java/phone/PhoneBook.java
@@ -0,0 +1,41 @@
+package com.java.phone;
+
+public class PhoneBook {
+
+	private String name;
+	private String hp;
+	private String tel;
+	
+	public PhoneBook(String name, String hp, String tel) {
+		super();
+		this.name = name;
+		this.hp = hp;
+		this.tel = tel;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getHp() {
+		return hp;
+	}
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	@Override
+	public String toString() {
+		return name+","+hp+","+tel;
+	
+	}
+}
+
